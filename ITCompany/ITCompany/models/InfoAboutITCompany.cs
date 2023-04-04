b909a20e2e14912cf196635981e3ba0b017d1a5e
dandad18 <dadalko1231@gmail.com>
2023-04-04 12:28:25 +0300

    Was added functional-class InfoAboutITCompany

---
+++ ITCompany/ITCompany/models/InfoAboutITCompany.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITCompany.models
+{
+    internal static class InfoAboutITCompany
+    {
+        public static short GetEmployeeCostSummary(ITCompany company)
+        {
+            short costs = 0;
+            foreach (var employee in company.Employees) costs += employee.Salary;
+            return costs;
+        }
+
+        public static List<Employee> FindEmployeesWithMaxSalary(ITCompany company)
+        {
+            var salaries = new List<short>();
+            foreach (var employee in company.Employees) salaries.Add(employee.Salary);
+            var maxSalary = salaries.Max();
+            return company.Employees.Where(e => e.Salary == maxSalary).ToList();
+        }
+
+        public static List<Employee> FindEmployeesWithMinSalary(ITCompany company)
+        {
+            var salaries = new List<short>();
+            foreach (var employee in company.Employees) salaries.Add(employee.Salary);
+            var minSalary = salaries.Min();
+            return company.Employees.Where(e => e.Salary == minSalary).ToList();
+        }
+    }
+}
