57cc2ac92e3fc5fda3e6e40a74841e758419dbb6
dandad18 <dadalko1231@gmail.com>
2023-04-04 12:30:21 +0300

    Was added class Menu for communication with user

---
+++ ITCompany/ITCompany/views/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITCompany.views
+{
+    internal static class Menu
+    {
+        public static void ShowActions()
+        {
+            // Реализация главного меню
+        }
+    }
+}
