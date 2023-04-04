dafd43c21c171b9d8088de5fb76365193fe4e5dd
dandad18 <dadalko1231@gmail.com>
2023-04-04 12:29:19 +0300

    Was added help-class ConsoleOutput

---
+++ ITCompany/ITCompany/views/ConsoleOutput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITCompany.views
+{
+    internal static class ConsoleOutput
+    {
+        public static void WriteLine(string msg, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void Write(string msg, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(msg);
+            Console.ResetColor();
+        }
+    }
+}
