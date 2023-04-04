73889e622c865755661569734252ab90d3445995
dandad18 <dadalko1231@gmail.com>
2023-04-04 12:26:00 +0300

    Was added entity-class Employee

---
+++ ITCompany/ITCompany/models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITCompany.models
+{
+    internal class Employee
+    {
+        public string Name { get; private set; }
+        public short Salary { get; private set; }
+
+        public Employee(string name, short salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
