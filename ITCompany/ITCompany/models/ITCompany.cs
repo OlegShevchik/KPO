d54ec56682536a59e9c8d97e55e4d752bcb1720e
dandad18 <dadalko1231@gmail.com>
2023-04-04 12:27:17 +0300

    was added entity-class ITCompany

---
+++ ITCompany/ITCompany/models/ITCompany.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITCompany.models
+{
+    internal class ITCompany
+    {
+        public string Name { get; private set; }
+
+        public List<Employee> Employees { get; private set; }
+
+        public ITCompany(string name) 
+        {
+            Name = name;
+            Employees = new List<Employee>();
+        }
+
+        public void AddEmployee(string name, short salary)
+        {
+            Employees.Add(new Employee(name, salary));
+        }
+
+        public void RemoveEmployee(Employee employee)
+        {
+            Employees.Remove(employee);
+        }
+    }
+}
